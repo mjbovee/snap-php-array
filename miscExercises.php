2546be65a358651de56282ce197a238369e38735
Michael Bovee <michael.j.bovee@gmail.com>
2018-10-20 15:46:24 -0600

    added part1 of exercise2

--- miscExercises.php
+++ miscExercises.php
@@ -5,16 +5,28 @@
  * Date: 10/20/18
  * Time: 2:33 PM
  */
-
-function isBitten() {
-	$bite = rand(1, 10);
-	if ($bite <= 5) {
+// first exercise
+function isBitten() : bool {
+	$bite = rand(1, 100);
+	if ($bite <= 50) {
 		return true;
+	} else {
+		return false;
 	}
 }
 
-if(isBitten()) {
+if(isBitten() === true) {
 	echo "Charlie bit your finger!";
 } else {
-	echo "Charlie did not bite your finger!";
+	echo "Charlie did not bite your finger! \n";
 }
+echo "<br/>";
+
+// second exercise
+function countWords($str) {
+	$words = preg_split('/[\s,]+/', $str);
+	$wordsCounted = (array_count_values($words));
+	var_dump($wordsCounted);
+}
+
+countWords( 'This is a string of words, it contains some words and it is a string');
